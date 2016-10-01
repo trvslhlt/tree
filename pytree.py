2d21a84629b3f06b1018f5a1b2c272ae237ee4cc
travis holt <trvslhlt@gmail.com>
2016-10-01 01:06:05 +0000

    tree without unicode

--- pytree.py
+++ pytree.py
@@ -8,6 +8,9 @@ import sys
 
 # determine number of inputs
 def validateInputs():
+    '''
+    validates the script's arguments. exits with error for invalid inputs
+    '''
     inputCount = len(sys.argv)
     if inputCount == 1:
         return # no argument passed
@@ -23,7 +26,11 @@ def validateInputs():
         print('please pass in 0 or 1 arguments')
         sys.exit(1)
 
+
 def findTreeRoot():
+    '''
+    returns the tree's root directory
+    '''
     inputCount = len(sys.argv)
     currentDirectoryPath = os.path.dirname(os.path.abspath(__file__))
     if inputCount == 1:
@@ -32,10 +39,35 @@ def findTreeRoot():
         return os.path.join(currentDirectoryPath, sys.argv[1])
 
 
+def buildTree(rootDirectory):
+    
+    def internalBuildTree(directory, level, lineHead, tree):
+        names = os.listdir(directory)
+        for idx, name in enumerate(names):
+            if name.startswith('.'):
+                continue
+            namepath = os.path.join(directory, name)
+            hatPipe = 'f'
+            if idx ==  len(names) - 1:
+                hatPipe = 'L'
+            tree =  tree + '\n' + lineHead +  hatPipe + '-- ' + name
+            if os.path.isdir(namepath):
+                descender = "   " if idx == (len(names) - 1) else "|  "
+                tree = internalBuildTree(namepath, level + 1, lineHead + descender, tree)
+        return tree
+   
+    tree = internalBuildTree(rootDirectory, 0, '', '.') 
+    return tree
+    
+
 def run():
+    '''
+    conducts the script
+    '''
     validateInputs()
     rootDirectory = findTreeRoot()
-    print(rootDirectory)
+    tree = buildTree(rootDirectory)
+    print(tree)
 
 if __name__ == '__main__':
     # just for demo
